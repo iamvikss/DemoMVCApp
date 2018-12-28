3fa34d654d8e96ae00403eed8c30cc96516a4453
Vikas <vikas.singh@live.com>
2018-12-28 09:44:11 +0530

    added custom routes

--- DemoMVCApp/App_Start/RouteConfig.cs
+++ DemoMVCApp/App_Start/RouteConfig.cs
@@ -13,11 +13,23 @@ namespace DemoMVCApp
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //Custom Routes
+            //MVC framework evaluates each route in sequence. It starts with first configured route and if incoming 
+            //url doesn't satisfy the URL pattern of the route then it will evaluate second route and so on.
+            
+            routes.MapRoute(
+                name: "Dashboard",
+                url: "dashboard",
+                defaults: new { controller = "MyHome", action = "Index" });
+
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "MyHome", action = "Index", id = UrlParameter.Optional }
             );
+
+            
         }
     }
 }
